ba7c41b08f5a0c0246041022316cb46c1e46a1b0
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-01 00:30:44 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,23 @@
 # Write your code here.
 def dictionary
-  
+  dictionary ={
+    "hello" => 'hi',
+    "to" => '2',
+    "two" => '2',
+    "too" => '2',
+    "for" => '4',
+    'four' => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  array_tweet = tweet.join
+  array_tweet.each do |word|
+    binding.pry
+    if word == dictionary.keys
+  end
 end
