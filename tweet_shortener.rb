26c7bc174e088c5d6263fb91bef81bd3806315b3
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-03-01 00:36:54 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,10 +18,13 @@ end
 
 def word_substituter(tweet)
   array_tweet = tweet.split
+  i = 0
   array_tweet.each do |word|
     binding.pry
     if dictionary.keys.include?(word)
-      
+      array_tweet[array_tweet.index(i)] = dictionary[word]
     end
+    i+=1
   end
+  print array_tweet
 end
